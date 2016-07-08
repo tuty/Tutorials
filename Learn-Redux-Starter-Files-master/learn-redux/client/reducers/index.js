039c525aad7d598f081af41cccc9e44eefaca6eb
Ivan Chervenkov <Ivan_Chervenkov@epam.com>
2016-07-08 19:02:09 +0300

    reduxstagram to 7 lesson

---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reducers/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ivan_Chervenkov on 7/8/2016.
+ */
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+    posts,
+    comments,
+    router: routerReducer
+});
+
+export default rootReducer;
---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reducers/posts.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ivan_Chervenkov on 7/8/2016.
+ */
+const posts = (state = [], action) => {
+    "use strict";
+    console.log(state, action);
+    return state;
+};
+
+export  default posts;
